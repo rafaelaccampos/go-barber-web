5cf0abf78d6a899677f8ef82bdb9ae25e20b7ee0
rafaelaccampos <rafaela-cassiano@hotmail.com>
2020-06-01 20:59:33 -0300

    feat: Add tooltip errors

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -7,9 +7,10 @@ import React, {
 } from 'react';
 
 import { IconBaseProps } from 'react-icons';
+import { FiAlertCircle } from 'react-icons/fi';
 import { useField } from '@unform/core';
 
-import { Container } from './styles';
+import { Container, Error } from './styles';
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   name: string;
@@ -47,7 +48,7 @@ const Input: React.FunctionComponent<InputProps> = ({
   }, [fieldName, registerField]);
 
   return (
-    <Container isFilled={isFilled} isFocused={isFocused}>
+    <Container isErrored={!!error} isFilled={isFilled} isFocused={isFocused}>
       {Icon && <Icon size={20} />}
       <input
         onFocus={handleInputFocus}
@@ -56,7 +57,11 @@ const Input: React.FunctionComponent<InputProps> = ({
         ref={inputRef}
         {...rest}
       />
-      {error}
+      {error && (
+        <Error title={error}>
+          <FiAlertCircle color="#c53030" size={20} />
+        </Error>
+      )}
     </Container>
   );
 };
--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -22,9 +22,9 @@ const SignUp: React.FunctionComponent = () => {
       const schema = Yup.object().shape({
         name: Yup.string().required('Nome obrigatório.'),
         email: Yup.string()
-          .required('E-mail obrigatório')
+          .required('E-mail obrigatório.')
           .email('Digite um e-mail válido.'),
-        password: Yup.string().min(6, 'No mínimo 6 dígitos'),
+        password: Yup.string().min(6, 'No mínimo 6 dígitos.'),
       });
 
       await schema.validate(data, {
