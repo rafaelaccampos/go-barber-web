86d66cce64662738d3404cadc6460ef4d78b0dfc
rafaelaccampos <rafaela-cassiano@hotmail.com>
2020-06-01 21:30:16 -0300

    feat: Add login validation

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 
-/* import SignIn from './pages/SignIn';
- */ import SignUp from './pages/SignUp';
-
+import SignIn from './pages/SignIn';
+/*  import SignUp from './pages/SignUp';
+ */
 import GlobalStyle from './styles/global';
 
 const App: React.FunctionComponent = () => (
   <>
-    {/*     <SignIn />
+    <SignIn />
+    {/*     <SignUp />
      */}
-    <SignUp />
     <GlobalStyle />
   </>
 );
