978e391ca59fc51dfc5b4c0e7e4b594b9ebd638c
rafaelaccampos <rafaela-cassiano@hotmail.com>
2020-07-07 21:16:41 -0300

    feat: Add and remove toasts

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -53,7 +53,12 @@ const SignIn: React.FunctionComponent = () => {
           formRef.current?.setErrors(errors);
         }
 
-        addToast();
+        addToast({
+          type: 'error',
+          title: 'Erro na autenticação',
+          description:
+            'Ocorreu um erro ao fazer o login, cheque as credenciais.',
+        });
       }
     },
     [signIn, addToast],
